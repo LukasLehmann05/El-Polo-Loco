96290d9a83b24963e170411bd2fd03e0aea527a1
Lukas Lehmann <lukaslehmann05@gmail.com>
2026-02-09 13:20:07 +0100

    fixed ratios and size of character and chickens

--- classes/character.class.js
+++ classes/character.class.js
@@ -3,9 +3,9 @@ class Character extends Moveable_object {
     constructor() {
         super().loadImage("../img/pepe/1_idle/idle/I-1.png")
 
-        this.height = 250
-        this.width = 150
-        this.pos_y = 400
+        this.height = 400
+        this.width = 250
+        this.pos_y = 260
     }
 
     jump() {
--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -4,7 +4,10 @@ class Chicken extends Moveable_object {
         super().loadImage("../img/enemies/chicken/chicken_normal/1_walk/1_w.png")
 
         this.pos_x = 300 + Math.random() * 700
-        this.pos_y = 500
+        this.pos_y = 520
+        this.height = 125
+        this.width = 100
+
     }
     
 }
